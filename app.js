e09e42b16a78885e64f4fe2002b0122e4042f460
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-11-02 19:16:24 +0300

    Changed css,html,and js files

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 
+
 const apiKey = '6tUUP5PEkz9G5ui55sAZ4lHhuDg5e4ekmVjl0HYQ';
 
 
 function fetchAPODForCurrentDate() {
     const apodUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-
-    fetch(apodUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            
-            const apodSection = document.getElementById('apod');
-            const apodImage = document.getElementById('apod-image');
-            const apodInfo = document.getElementById('apod-info');
-            
-            apodImage.src = data.url;
-            apodImage.alt = data.title;
-
-            apodInfo.innerHTML = `
-                <h3>${data.title}</h3>
-                <p>Date: ${data.date}</p>
-                <p>${data.explanation}</p>
-            `;
-        })
-        .catch((error) => console.error('Error fetching APOD:', error));
-}
-
-
-function fetchAPODForSpecificDate(selectedDate) {
-    const apodUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${selectedDate}`;
 
     fetch(apodUrl)
         .then((response) => response.json())
@@ -78,61 +55,49 @@ function fetchMarsRoverPhotos(selectedDate, selectedRover, selectedCamera) {
 }
 
 
-function handleScroll() {
-    const apodSection = document.getElementById('apod');
-    const marsRoverSection = document.getElementById('mars-rover-photos');
-    const spaceNewsSection = document.getElementById('space-news');
-
-    
-    const scrollPosition = window.scrollY;
-
-    
-    if (scrollPosition < apodSection.offsetHeight) {
-        
-        fetchAPODForCurrentDate();
-    } else if (scrollPosition < apodSection.offsetHeight + marsRoverSection.offsetHeight) {
-       
-    } else {
-        
-    }
-}
 
 
 function handleUserInteractions() {
     const randomButton = document.getElementById('random-button');
-    const searchButton = document.getElementById('search-button');
     const exploreMarsButton = document.getElementById('explore-mars-button');
-    const marsForm = document.getElementById('mars-form');
+    const previousAPODButton = document.getElementById('previous-apod-button');
+    const apodForm = document.getElementById('apod-form');
 
     randomButton.addEventListener('click', fetchAPODForCurrentDate);
-    
-    searchButton.addEventListener('click', () => {
-        const selectedDate = document.getElementById('apod-date').value;
-        fetchAPODForSpecificDate(selectedDate);
-    });
 
     exploreMarsButton.addEventListener('click', () => {
+        const marsForm = document.getElementById('mars-form');
         marsForm.style.display = 'block';
         const marsRoverPhotosSection = document.getElementById('mars-rover-photos');
         marsRoverPhotosSection.style.display = 'none';
     });
-    
-    marsForm.addEventListener('submit', (event) => {
-        event.preventDefault();
 
-        const selectedDate = document.getElementById('mars-date').value;
-        const selectedRover = document.getElementById('rover-selection').value;
-        const selectedCamera = document.getElementById('camera-selection').value;
-
-        fetchMarsRoverPhotos(selectedDate, selectedRover, selectedCamera);
+    previousAPODButton.addEventListener('click', () => {
+        const apodForm = document.getElementById('apod-form');
+        apodForm.style.display = 'block';
+        const marsForm = document.getElementById('mars-form');
+        marsForm.style.display = 'none';
     });
+
+    apodForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const selectedDate = document.getElementById('apod-date').value;
+        fetchAPODForSpecificDate(selectedDate);
+    });
+
+    
+    function handleScroll() {
+        const apodSection = document.getElementById('apod');
+        const marsRoverSection = document.getElementById('mars-rover-photos');
+        const spaceNewsSection = document.getElementById('space-news');
+    }
+
+    
+    window.addEventListener('scroll', handleScroll);
 }
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('scroll', handleScroll);
-    handleUserInteractions();
+document.addEventListener('DOMContentLoaded', handleUserInteractions);
 
-    
-    fetchAPODForCurrentDate();
-});
+
+fetchAPODForCurrentDate();
