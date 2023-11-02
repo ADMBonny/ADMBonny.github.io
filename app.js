8f5739ea071afa0245d35f8223d04b74bb9baaa1
Bonventure Maeta Oanda <64786006+ADMBonny@users.noreply.github.com.>
2023-11-02 14:59:39 +0300

    Defined a function to fetch current data from APOD API.

--- app.js
+++ app.js
@@ -2,3 +2,23 @@
 const apiKey = '6tUUP5PEkz9G5ui55sAZ4lHhuDg5e4ekmVjl0HYQ';
 
 
+
+function fetchAPODForCurrentDate() {
+    const apodUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+    fetch(apodUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            
+            const apodSection = document.getElementById('apod');
+            apodSection.innerHTML = `
+                <h2>${data.title}</h2>
+                <img src="${data.url}" alt="${data.title}">
+                <p>${data.explanation}</p>
+            `;
+        })
+        .catch((error) => console.error('Error fetching APOD:', error));
+}
+
+
+
